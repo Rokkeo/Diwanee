89591ccc861cad17af6426fedd963cc0f2177796
Rokke <v.topolovacki@gmail.com>
2017-04-17 12:20:32 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-//$(document).ready(function(){
-//	$('.hamburger').click(function(){
-//        $('.header-nav').css("visibility","visible");
-//		
-//    $('.hamburger').click(function(){
-//        $('.header-nav').css("visibility","hidden");
-//    });
-//    
-//    });
-//});
 
+// opens main navigation in responsive mode
 $(".hamburger").click(function(){
     $(".header-nav, .nav-social").toggleClass("is-visible");
 });
